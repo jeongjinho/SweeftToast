bb952a7cc23d5adaca72cc10ba8630f9c2c12c74
jeong jin ho <yyyqor@gmail.com>
2019-03-05 16:15:24 +0900

    add multiLine function to Sources file

--- Examples/Toast/Toast/UI_Extension/UITableView_Extension.swift
+++ Examples/Toast/Toast/UI_Extension/UITableView_Extension.swift
@@ -6,4 +6,22 @@
 //  Copyright © 2019 jeongjinho. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+protocol CellReusable {
+    static var reuseIdentifier: String { get }
+}
+extension UITableViewCell: CellReusable {
+    static var reuseIdentifier: String {
+        return String(describing: self.self)
+    }
+}
+
+extension UITableView {
+    func register<T>(cell: T.Type) where T: UITableViewCell {
+        register(cell, forCellReuseIdentifier: cell.reuseIdentifier)
+    }
+    func registerForNib<T>(cell: T.Type) where T: UITableViewCell {
+        register(UINib(nibName: cell.reuseIdentifier, bundle: nil), forCellReuseIdentifier: cell.reuseIdentifier)
+    }
+}
