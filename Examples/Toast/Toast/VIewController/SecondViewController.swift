9411d89ee52ab4f0f4586794b4b1eebe044ca43e
Jzinos <jafreecazinos@gmail.com>
2019-06-10 16:54:54 +0900

    [#4]   add ClickAction in Toast
    This reverts commit e757c154fdd0e7c23885edf1f7a78d0106bd2faa.

---
+++ Examples/Toast/Toast/VIewController/SecondViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecondViewController.swift
+//  Toast
+//
+//  Created by jeongjinho on 11/06/2019.
+//  Copyright © 2019 jeongjinho. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
